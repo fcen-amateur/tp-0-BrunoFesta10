75b8ccc171e187c658b9b633403af0d2c7d0d8b5
BrunoFesta10 <133234805+BrunoFesta10@users.noreply.github.com>
2024-04-26 13:51:23 -0300

    Update archivoQueMePiden.py
    aca esta el plot definitivo

--- plotters/archivoQueMePiden.py
+++ plotters/archivoQueMePiden.py
@@ -2,21 +2,25 @@ import seaborn.objects as so
 from gapminder import gapminder
 
 def plot():
-    figura2 = (
+    figura = (
         so.Plot(
             gapminder[gapminder["pop"]<1000000],
             x="lifeExp",
             y="gdpPercap",
-            color="year",
         )
-        .add(so.Line())
+        .add(so.Line(color='red', linewidth=3), so.PolyFit(1))
+	.add(so.Dot()).scale(y="log")
         .label(
             title="Expectativa de vida respecto a los ingresos en paises pequeños",
             x="Expectativa de Vida",
             y="Ingresos Per Capita",
-            color="Año",
         )
               )
+    return dict(
+        descripcion="Expectativa de vida respecto a los ingresos en paises pequeños durante los años",
+        autor="Bruno Festa",
+        figura=figura,
+    )
     return dict(
         descripcion="Expectativa de vida respecto a los ingresos en paises pequeños",
         autor="Bruno Festa",

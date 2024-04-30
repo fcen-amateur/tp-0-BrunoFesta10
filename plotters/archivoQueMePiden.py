f0132531afa53d5e82a7b2e0d81d11b25ed9ce49
BrunoFesta10 <133234805+BrunoFesta10@users.noreply.github.com>
2024-04-30 16:29:27 -0300

    cambio el archivo por el que no tiene fiigra2

--- plotters/archivoQueMePiden.py
+++ plotters/archivoQueMePiden.py
@@ -1,47 +1,24 @@
 import seaborn.objects as so
 from gapminder import gapminder
 
-
 def plot():
     figura = (
-        so.Plot(
-            gapminder[gapminder.continent == "Oceania"],
-            x="year",
-            y="lifeExp",
-            color="country",
-        )
-        .add(so.Line())
-        .label(
-            title="Expectativa de vida en Oceanía",
-            x="Año",
-            y="Expectativa de vida",
-            color="País",
-        )
-    )
-    return dict(
-        descripcion="Expectativa de vida en países de Oceanía a lo largo del tiempo",
-        autor="La cátedra",
-        figura=figura,
-    )
-
-def plot():
-    figura2 = (
         so.Plot(
             gapminder[gapminder["pop"]<1000000],
             x="lifeExp",
             y="gdpPercap",
-            color="year",
         )
-        .add(so.Line())
+        .add(so.Line(color='red', linewidth=3), so.PolyFit(1))
+	.add(so.Dot()).scale(y="log")
         .label(
             title="Expectativa de vida respecto a los ingresos en paises pequeños",
             x="Expectativa de Vida",
             y="Ingresos Per Capita",
-            color="Año",
         )
               )
     return dict(
-        descripcion="Expectativa de vida respecto a los ingresos en paises pequeños",
+        descripcion="Expectativa de vida respecto a los ingresos en paises pequeños durante los años",
         autor="Bruno Festa",
-        figura2=figura2,
+        figura=figura,
     )
+
